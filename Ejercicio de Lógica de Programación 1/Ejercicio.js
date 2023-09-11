0572ceb559d2c0bdd11f0abdc3bf1f85ea0efc11
AlanOGxD <alanosvaldo88@gmail.com>
2023-09-11 16:17:07 -0600

    Cambio impresion, finalizado

--- Ejercicio de Lógica de Programación 1/Ejercicio.js
+++ Ejercicio de Lógica de Programación 1/Ejercicio.js
@@ -21,40 +21,19 @@ for (let i = 0; i < 3; i++) {
 
 }
 //Cadena antes de ordenar
-let cadena1 = "";
-arreglo.forEach(element => {
-    cadena1 += (element + " ");
-});
 
+console.log("Cadena desordenada: " + arreglo);
 
 //Metodo de ordenamiento mayor a menor
 arreglo.sort(function (a, b) { return b - a });
 
 //Cadena de datos ordenada de mayor a menor
-let cadenaMayorMenor = "";
-arreglo.forEach(element => {
-    cadenaMayorMenor += (element + " ");
-});
+console.log("Cadena ordenada de mayor a menor: " + arreglo);
 
 //Metodo de ordenamiento menor a mayor
 arreglo.sort(function (a, b) { return a - b });
 
 //Cadena de datos ordenada de menor a mayor
-let cadenaMenorMayor = "";
-arreglo.forEach(element => {
-    cadenaMenorMayor += (element + " ");
-});
 
+console.log("Cadena Ordenada de menor a mayor: " + arreglo);
 
-
-//Impresiones de pantalla
-console.log("Cadena desordenada: ");
-console.log(cadena1);
-
-
-console.log("Cadena ordenada de mayor a menor: ");
-console.log(cadenaMayorMenor);
-
-
-console.log("Cadena Ordenada de menor a mayor: ");
-console.log(cadenaMenorMayor);
